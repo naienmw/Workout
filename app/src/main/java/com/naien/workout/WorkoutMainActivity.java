882dec0dc05ba10521c1e1d2f99fda3f2b9a8683
naienmw <naien.mw@gmail.com>
2016-01-03 21:56:22 +0100

    save and get images to/from database

--- app/src/main/java/com/naien/workout/WorkoutMainActivity.java
+++ app/src/main/java/com/naien/workout/WorkoutMainActivity.java
@@ -17,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -28,6 +29,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 import fr.tvbarthel.lib.blurdialogfragment.BlurDialogFragment;
@@ -46,9 +48,13 @@ public class WorkoutMainActivity extends Activity{
     String PrimaryWorkout;
     ImageView ExPic;
     RelativeLayout RelLayoutChoice;
-    ListView ListViewChoice;
+    ExpandableListView ListViewChoice;
     Animation faboben;
     Boolean toolbarisshown = false;
+
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    ExpListAdapter explistadapter;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +80,12 @@ public class WorkoutMainActivity extends Activity{
         PrimaryWorkout = getPrimaryWorkout(the_workout);
         ExPic = (ImageView)findViewById(R.id.picture_exercise);
 
-        ListViewChoice = (ListView)findViewById(R.id.listview_exercises_choice);
+        ListViewChoice = (ExpandableListView)findViewById(R.id.listview_exercises_choice);
+         temp_list_data();
+        explistadapter = new ExpListAdapter(this,listDataHeader,listDataChild);
+
+        ListViewChoice.setAdapter(explistadapter);
+
         RelLayoutChoice = (RelativeLayout) findViewById(R.id.rellayout_exchoice);
 
         switch(PrimaryWorkout) {
@@ -141,6 +152,44 @@ public class WorkoutMainActivity extends Activity{
         );
     }
 
+    private void temp_list_data(){
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
 
 
     public void AddNewExercise(View view) {
